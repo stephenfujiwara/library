fc1b0dd70278cc71af541cdbe764ca939bb945ad
stephenfujiwara <stephenfujiwara456@gmail.com>
2022-12-11 15:45:51 -0800

    move files around

--- server/src/models/Author.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-
-// mongodb assigns a unique id to each collection, no need to add it
-const authorSchema = new Schema({
-  name: String,
-});
-
-export const Author = model("Author", authorSchema);
--- server/src/models/Book.js
+++
@@ -1,9 +0,0 @@
-import { model, Schema } from "mongoose";
-
-const bookSchema = new Schema({
-  name: String,
-  genre: String,
-  authorName: String,
-});
-
-export const Book = model("Book", bookSchema);
